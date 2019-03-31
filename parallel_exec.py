6d969942054448a617033ab5998a58d4243f1107
David Dodel <david.dodel@hotmail.com>
2019-03-31 01:00:40 +0100

    Working concurrent execution using Popen

---
+++ parallel_exec.py
@@ -0,0 +1,11 @@
+from subprocess import Popen, PIPE
+import server
+
+def main():
+  cmds_list  = [['/home/ddodel/miniconda3/bin/python', 'server.py'],['/home/ddodel/Documents/github-repos/PCLServer/build/server']]
+  procs_list = [Popen(cmd, stdout=PIPE, stderr=PIPE) for cmd in cmds_list]
+  for proc in procs_list:
+    proc.wait()
+
+if __name__ == "__main__":
+  main()
--- server.py
+++ server.py
@@ -1,20 +1,25 @@
 import socket
 import sys
 import struct
+import subprocess
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main():
 
-# Bind the socket to the port
-server_address = ('127.0.0.1', 4444)
-#server_address = ('localhost', 4444)
-print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+  # Create a TCP/IP socket
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Listen for incoming connections
-sock.listen(1)
+  # Bind the socket to the port
+  server_address = ('127.0.0.1', 4445)
+  print('starting up on {} port {}'.format(*server_address))
+  sock.bind(server_address)
 
-while True:
+  # Start PCLServer
+  #subprocess.call("/home/ddodel/Documents/github-repos/PCLServer/build/server")
+
+  # Listen for incoming connections
+  sock.listen(1)
+
+  while True:
     # Wait for a connection
     print('waiting for a connection')
     connection, client_address = sock.accept()
@@ -23,21 +28,25 @@ while True:
 
         # Receive the data in small chunks and retransmit it
         while True:
-            # Receive the size of the PCL
+            # Receive the size of the PCL (size_t)
             data = connection.recv(8)
             if data:
               # Check endianess of PC with: `lscpu | grep "Byte Order"`
               pcl_size = int.from_bytes(data, byteorder='little')
               print('Received pcl of size: {!r}'.format(pcl_size))
-            
+              
+
               for i_pt in range(0, pcl_size):
                 #print(i_pt)
+                # Receive single points, represented by floats
                 x = struct.unpack('f', connection.recv(4))
                 y = struct.unpack('f', connection.recv(4))
                 z = struct.unpack('f', connection.recv(4))
 
-                print((x, y, z))
-              print('Finished first pcl')
+                #print((x, y, z))
+              # Do stuff with PCL using SPFN
+              # SPFN output still has to be converted to JSON? to more easily be imported in UE4
+              print('PCL saved')
             else:
                 print('no data from', client_address)
                 break
@@ -45,3 +54,6 @@ while True:
     finally:
         # Clean up the connection
         connection.close()
+
+if __name__ == '__main__':
+  main()
